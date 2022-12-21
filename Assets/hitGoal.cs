4e41403e5e138e8d1ebb9129425ddfdda5930114
xuekee1207 <kiewxuekee@graduate.utm.my>
2022-12-21 22:28:46 +0800

    ending UI

---
+++ Assets/hitGoal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitGoal : MonoBehaviour
+{
+    public GameObject endCanva;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        endCanva.SetActive(true);
+        Debug.Log("hit goal");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        endCanva.SetActive(true);
+        Debug.Log("hit goal");
+    }
+    
+    
+}
